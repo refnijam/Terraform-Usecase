f9ce45f3d878dd0a9cd9e6ed3f317900bc4906c9
refnijam <89676903+refnijam@users.noreply.github.com>
2022-12-29 20:12:10 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,42 @@
+resource "aws_vpc_ipv4_cidr_block_association" "secondary_cidr" {
+  vpc_id              = var.aws_vpc
+  cidr_block          = var.secondary_cidr
+}
+
+resource "aws_subnet" "private" {
+  count = length(var.private_subnets) > 0 ? length(var.private_subnets) : 0
+
+  vpc_id               = var.aws_vpc
+  cidr_block           = var.private_subnets[count.index]
+  availability_zone    = length(regexall("^[a-z]{2}-", element(var.availability_zones, count.index))) > 0 ? element(var.availability_zones, count.index) : null
+  availability_zone_id = length(regexall("^[a-z]{2}-", element(var.availability_zones, count.index))) == 0 ? element(var.availability_zones, count.index) : null
+
+  tags = {
+      "Name" = format(
+        "${var.project_name}-private-sn-%s",
+        element(var.availability_zones, count.index),
+      )
+    }
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = var.aws_vpc
+   route {
+    cidr_block        = "0.0.0.0/0"
+    gateway_id        = var.shared_transit_gateway_id
+  }
+  tags = {
+    "Name" = "${var.project_name}-private-route-table"
+  }
+}
+
+resource "aws_route_table_association" "private" {
+  count = length(var.private_subnets) > 0 ? length(var.private_subnets) : 0
+
+  subnet_id            = element(aws_subnet.private[*].id, count.index)
+  route_table_id       = aws_route_table.private.id
+} 
+
+  data "aws_ec2_transit_gateway_vpc_attachment" "tgw_attachment" {
+       id = ""
+  }
